d3c3d37af6a94e8a2031906a8a0e9cd169fba27d
Steven Mak <stevenmak@gmail.com>
2024-07-30 12:25:18 +0100

    extract width as member variable

--- app/src/main/java/tictactoe/Board.java
+++ app/src/main/java/tictactoe/Board.java
@@ -5,19 +5,21 @@ import java.util.List;
 
 public class Board {
     private List<Character> board = new ArrayList<Character>();
-
-    public Board (List<Character> board) {
-        this.board = board;
-    }
+    private int width = 0;
 
     public Board (char[][] board) {
         for (char[] row: board) 
             for (char cell: row) 
                 this.board.add(cell);
+        setWidth();
+    }
+
+    private void setWidth() {
+        this.width = (int)Math.sqrt(board.size());
     }
 
     public int getWidth() {
-        return (int)Math.sqrt(board.size());
+        return this.width;
     }
 
     public Character getCell(int row, int col) {
